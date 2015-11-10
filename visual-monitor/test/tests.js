76f618956ba1fffc4477e76edb6f865d58334a4a
David Kohav <david@gizra.com>
2015-11-10 10:34:23 +0200

    Squashed #1 : Add visual monitor.
    commit afe6aa0fd48ddbd3ab87740ccd83a4c0452959f4
    Author: David Kohav <david@gizra.com>
    Date:   Tue Nov 10 10:31:59 2015 +0200
    
        Add visual monitor.

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -63,10 +63,7 @@ describe('Visual monitor testing', function() {
       .url(baseUrl)
       .webdrivercss(testName + '.homepage', {
         name: '1',
-        exclude: [],
-        remove: [],
-        hide: [],
-        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+        screenWidth: selectedCaps == 'chrome' ? [960] : undefined,
       }, resultsCallback)
       .call(done);
   });
